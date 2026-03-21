e52af360c5794e08c6dc767c0603d18e0ec4533b
josuanhub <urusgovx@gmail.com>
2026-03-21 14:29:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const express = require("express");
 const urusScoutRoutes = require("./routes/urus_scout.routes");
 const moltbookPublishRoutes = require("./routes/moltbook_publish.routes");
