611b45a8b4db960b0616067e6100da8ba8eeb604
josuanhub <urusgovx@gmail.com>
2026-03-21 00:10:34 -0400

    Update urus_scout.routes.js

--- routes/urus_scout.routes.js
+++ routes/urus_scout.routes.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/scout", async (req, res) => {
-  return res.json({
-    ok: true,
-    route: "urus_scout",
-    message: "Ruta scout activa"
-  });
-});
+const { scout } = require("./controllers/urus_scout.controller");
+
+router.post("/scout", scout);
 
 module.exports = router;
