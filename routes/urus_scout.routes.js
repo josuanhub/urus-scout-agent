61066fb3e6f90b4b47d93ffd75a18e23cfa32125
josuanhub <urusgovx@gmail.com>
2026-03-21 02:38:17 -0400

    Update urus_scout.routes.js

--- routes/urus_scout.routes.js
+++ routes/urus_scout.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { scout } = require("./controllers/urus_scout.controller");
+const { scout, status } = require("./controllers/urus_scout.controller");
 
 router.post("/scout", scout);
+router.get("/status", status);
 
 module.exports = router;
