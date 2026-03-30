9f237cb1650345aec7aa17469aef9aa06ebd0b19
josuanhub <urusgovx@gmail.com>
2026-03-30 00:16:10 -0400

    Update urus_scout.routes.js

--- routes/urus_scout.routes.js
+++ routes/urus_scout.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { scout, status, leaderboard } = require("./controllers/urus_scout.controller");
+const { scout, status, topAgents } = require("./controllers/urus_scout.controller");
 
 router.post("/scout", scout);
 router.get("/status", status);
 
 // 👇 ESTE ES EL NUEVO
 router.get("/leaderboard", leaderboard);
+router.get("/top-agents", topAgents);
 
 module.exports = router;
